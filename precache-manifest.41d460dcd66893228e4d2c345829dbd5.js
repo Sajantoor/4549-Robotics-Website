ad6d25f4ac01e6d88b3c0629364cd0806e723145
Sajantoor98 <Sajantoor98@gmail.com>
2019-09-21 16:10:02 -0700

    Updates

---
+++ precache-manifest.41d460dcd66893228e4d2c345829dbd5.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "982b3486b4e8023457e779765c5d4d22",
+    "url": "/envertronics/index.html"
+  },
+  {
+    "revision": "896b248032948f2668c0",
+    "url": "/envertronics/static/css/main.ddf8e977.chunk.css"
+  },
+  {
+    "revision": "253b7ef89cef62930618",
+    "url": "/envertronics/static/js/2.379f1beb.chunk.js"
+  },
+  {
+    "revision": "896b248032948f2668c0",
+    "url": "/envertronics/static/js/main.69a0650d.chunk.js"
+  },
+  {
+    "revision": "1b0ddb8199700c1eae94",
+    "url": "/envertronics/static/js/runtime~main.d4e68a1a.js"
+  },
+  {
+    "revision": "267db4a3117d3d87d762e9a07b89538c",
+    "url": "/envertronics/static/media/Akrobat-Black.267db4a3.woff"
+  },
+  {
+    "revision": "150b3ea27a9de6fdadee12026205b96c",
+    "url": "/envertronics/static/media/Akrobat-Bold.150b3ea2.woff"
+  },
+  {
+    "revision": "fba895d6a6ec9d3f799651acdcfac991",
+    "url": "/envertronics/static/media/Akrobat-ExtraLight.fba895d6.woff"
+  },
+  {
+    "revision": "9e3c991595a7019b8c5c43636b308762",
+    "url": "/envertronics/static/media/Akrobat-Light.9e3c9915.woff"
+  },
+  {
+    "revision": "456846fcb3a2aa0ca09c6e8ccf5ea112",
+    "url": "/envertronics/static/media/Akrobat-Regular.456846fc.woff"
+  },
+  {
+    "revision": "b106cbfe5e0099e4a08db2a03d572d97",
+    "url": "/envertronics/static/media/Akrobat-SemiBold.b106cbfe.woff"
+  },
+  {
+    "revision": "b3817e9b17fc658ed526820a1d07c766",
+    "url": "/envertronics/static/media/akrobat-extrabold-webfont.b3817e9b.woff"
+  },
+  {
+    "revision": "519201923e903b8fb2c0834426fa3192",
+    "url": "/envertronics/static/media/instagramLogo.51920192.svg"
+  },
+  {
+    "revision": "aaaf1dee5cc6afbb64d468def749c1cf",
+    "url": "/envertronics/static/media/logo.aaaf1dee.svg"
+  },
+  {
+    "revision": "472abf0f626b5e3bc7bc17d6bbd2d7e7",
+    "url": "/envertronics/static/media/navDown.472abf0f.svg"
+  },
+  {
+    "revision": "95d58da78e1a01c1ae9f1f7e57c9957d",
+    "url": "/envertronics/static/media/stars.95d58da7.png"
+  }
+]);
